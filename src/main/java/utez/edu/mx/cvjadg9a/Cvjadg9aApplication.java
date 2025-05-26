4a148678f93b235dec06c785eddaac704afa07f4
Antonie <20223TN047@utez.edu.mx>
2025-05-26 17:44:05 -0600

    [FIRST COMMIT] Subida inicial del proyecto

---
+++ src/main/java/utez/edu/mx/cvjadg9a/Cvjadg9aApplication.java
@@ -0,0 +1,13 @@
+package utez.edu.mx.cvjadg9a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cvjadg9aApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cvjadg9aApplication.class, args);
+    }
+
+}
---
+++ src/test/java/utez/edu/mx/cvjadg9a/Cvjadg9aApplicationTests.java
@@ -0,0 +1,13 @@
+package utez.edu.mx.cvjadg9a;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cvjadg9aApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
