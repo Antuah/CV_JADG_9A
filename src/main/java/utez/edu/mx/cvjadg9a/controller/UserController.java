bd99ddb2ce5e9e47325249f3d6778a54ceba48f4
Antonie <20223TN047@utez.edu.mx>
2025-05-26 18:07:25 -0600

    [FIRST COMMIT] Subida inicial del proyecto

---
+++ src/main/java/utez/edu/mx/cvjadg9a/controller/UserController.java
@@ -0,0 +1,9 @@
+package utez.edu.mx.cvjadg9a.controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+public class UserController {
+    
+}
